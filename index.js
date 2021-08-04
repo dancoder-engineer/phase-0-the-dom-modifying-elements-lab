c5825448bc73fcb984d2ad898fa5c1994e14b7c5
Dan Seminara <japaneseforgaming@gmail.com>
2021-08-04 14:38:55 -0400

    Assignment Complete!

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const kill = document.querySelector("#main");
+kill.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id="victory";
+newHeader.innerHTML="Dan is the champion";
+
+document.body.appendChild(newHeader);
